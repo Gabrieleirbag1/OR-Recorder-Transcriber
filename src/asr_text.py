10f9e5326d24c40d4ab6801a95eb40cb1dceed85
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-06-19 15:01:38 -0400

    asr to text feat + labelisation

--- src/asr_text.py
+++ src/asr_text.py
@@ -2,34 +2,27 @@ import whisper
 import os
 from utils import OUTPUT_DIR
 
+from nol_event_classifier.supervised.supervised_clustering import match_events_to_labels, RAW_LABELS
+
+def process_audio_to_label(file_path, model_name_asr="base", model_name_embedding="paraphrase-multilingual-mpnet-base-v2"):
+    text = transcribe_audio(file_path, model_name_asr)
+    print(f"Transcrit : '{text}'")
+
+    if not text:
+        return None
+
+    results, _ = match_events_to_labels([text], RAW_LABELS, model_name_embedding, top_k=3)
+    return results[0]
+
 def transcribe_audio(file_path, model_name="base"):
     model = whisper.load_model(model_name)
     result = model.transcribe(file_path)
     return result["text"]
 
-def best_model_test(file_path):
-    model_names = ["tiny", "base", "small", "medium"]
-    best_model = None
-    best_score = float("inf")
-    
-    for model_name in model_names:
-        print(f"Testing model: {model_name}")
-        model = whisper.load_model(model_name)
-        result = model.transcribe(file_path)
-        score = len(result["text"])  # Simple heuristic: shorter transcription might indicate better accuracy
-        print(f"Model: {model_name}, Transcription: {result['text']}, Score: {score}")
-        
-        if score < best_score:
-            best_score = score
-            best_model = model_name
-            
-    print(f"Best model: {best_model} with score: {best_score}")
-
 if __name__ == "__main__":
     audio_file = os.path.join(OUTPUT_DIR, "output.wav")
     if os.path.exists(audio_file):
-        transcription = transcribe_audio(audio_file, "medium")
-        # best_model_test(audio_file)
-        print("Transcription:", transcription)
+        result = process_audio_to_label(audio_file, "base")
+        print("Résultat de classification:", result)
     else:
         print(f"Audio file '{audio_file}' not found. Please record audio first.")
