6bc8e1d139b29d66b81e1cff848c4af2ecfd08bf
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-06-22 09:58:18 -0400

    add inital prompt context to fine the transcription model

--- src/asr_text.py
+++ src/asr_text.py
@@ -1,7 +1,11 @@
+import json
+import os
 import whisper
 from utils import OUTPUT_DIR
-
 from nol_event_classifier.supervised.supervised_clustering import match_events_to_labels, RAW_LABELS
+
+with open(os.path.join(os.path.dirname(__file__), "medical_context.json"), "r", encoding="utf-8") as f:
+    MEDICAL_CONTEXT = ' '.join(json.load(f))
 
 def process_audio_to_label(file_path, model_name_asr="base", model_name_embedding="paraphrase-multilingual-mpnet-base-v2"):
     text = transcribe_audio(file_path, model_name_asr)
@@ -15,14 +19,14 @@ def process_audio_to_label(file_path, model_name_asr="base", model_name_embeddin
 
 def transcribe_audio(file_path, model_name="base"):
     model = whisper.load_model(model_name)
-    result = model.transcribe(file_path)
+    result = model.transcribe(file_path, initial_prompt=MEDICAL_CONTEXT)
     return result["text"]
 
 if __name__ == "__main__":
     import os
     audio_file = os.path.join(OUTPUT_DIR, "output.wav")
     if os.path.exists(audio_file):
-        result = process_audio_to_label(audio_file, "base")
+        result = process_audio_to_label(audio_file, "tiny")
         print("Résultat de classification:", result)
     else:
         print(f"Audio file '{audio_file}' not found. Please record audio first.")
