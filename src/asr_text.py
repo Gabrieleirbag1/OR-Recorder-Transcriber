0e735a4b704f991d67ac767c9345c5a81e18cfc1
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-07-06 11:42:14 -0400

    refactor: enhance AudioProcessor to support event logging and model loading

--- src/asr_text.py
+++ src/asr_text.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import whisper
 from lite_logging.lite_logging import log
 from utils import AUDIO_DIR, THRESHOLD
@@ -10,15 +11,19 @@ with open(os.path.join(os.path.dirname(__file__), "medical_context.json"), "r", 
     MEDICAL_CONTEXT = ' '.join(json.load(f))
 
 class AudioProcessor:
-    def __init__(self, model_name_asr="base", model_name_embedding="paraphrase-multilingual-mpnet-base-v2", gui=False):
+    def __init__(self, model_name_asr="base", model_name_embedding="paraphrase-multilingual-mpnet-base-v2", gui=False, event_logger=False):
         self.asr_model_name = model_name_asr
         self.embedding_model_name = model_name_embedding
         self.gui = gui
         self.asr_model = None
         self.supervised_clustering = None
-        self.event_logger = None
+        self.event_logger = EventLoggerCSV() if event_logger else None
         self.best_event: dict = {}
         self.events: list[dict] = [{}]
+
+    def load_models(self):
+        self.load_asr_model()
+        self.load_embedding_model()
 
     def load_asr_model(self):
         self.asr_model = whisper.load_model(self.asr_model_name)
@@ -37,7 +42,7 @@ class AudioProcessor:
     
     def process_audio_to_label(self, file_path) -> dict | None:
         text = self.transcribe_audio(file_path)
-        text = "chaussettes de ouf"
+        text = "dlzeofdnazeoidhazoedouazhdauozehuazhduhaezudhaezhdazeu"
         log(f"Transcribed : '{text}'")
 
         if not text:
@@ -47,10 +52,25 @@ class AudioProcessor:
         self.events = results[0]["top_k"]
         log("Classification results: " + str(results[0]))
         return results[0]
+    
+    def log_classification_results(self, result, corrected_label=None):
+        if self.event_logger:
+            text = result["event_raw"] #propofol 0.05 mg 
+            dose = re.search(r'(\d+(\.\d+)?)\s*(mg|g|ml|l|units)?', text)
+            dose = dose.group(0) if dose else "N/A"
+
+            self.event_logger.append_to_csv_file(
+                event=result["event_raw"],
+                dose=dose,
+                event_type="N/A",
+                selected_label=result["top_k"][0]["label"],
+                score=result["top_k"][0]["score"],
+                corrected_label=corrected_label
+            )
 
     def handle_label_selection(self, result):
         best_score = float(result["best_score"])
-        if best_score >= THRESHOLD and not self.gui:
+        if best_score <= THRESHOLD and not self.gui:
             print("Please select the most appropriate label from the following options:")
             for (i, event) in enumerate(result["top_k"]):
                 print(f"[{i+1}] Label: {event['label']}, Score: {event['score']}")
@@ -61,11 +81,13 @@ class AudioProcessor:
                     if 0 <= selected_index < len(result["top_k"]):
                         selected_label = result["top_k"][selected_index]
                         log(f"User selected label: {selected_label}")
+                        self.log_classification_results(result, corrected_label=selected_label["label"])
                         return selected_label
                     else:
                         log("Invalid selection. No label selected.")
                 else:
                     log("No label selected by user.")
+        self.log_classification_results(result)
         return result["top_k"][0]
 
     def evaluate_audio_event(self, file_path):
@@ -80,7 +102,8 @@ class AudioProcessor:
 if __name__ == "__main__":
     audio_file = os.path.join(AUDIO_DIR, "output.wav")
     if os.path.exists(audio_file):
-        audio_processor = AudioProcessor()
+        audio_processor = AudioProcessor(event_logger=True)
+        audio_processor.load_models()
         result = audio_processor.evaluate_audio_event(audio_file)
         log(f"Best event: {result['label']} (score: {result['score']:.2f})")
     else:
