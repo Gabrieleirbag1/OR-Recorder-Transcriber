1491f331655a068f5bd4655d6906f8b567e5058c
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-07-06 11:35:48 -0400

    refactor: allow configurable output directory for CSV file creation in EventLoggerCSV

--- src/event_logger.py
+++ src/event_logger.py
@@ -8,9 +8,18 @@ import datetime
 from utils import DATA_DIR
 
 class EventLoggerCSV:
-    def __init__(self):
+    def __init__(self, output_dir=DATA_DIR):
+        self.output_dir = output_dir
         self.file_path = None
         self.create_csv_file()
+
+    def create_csv_file(self):
+        # filename must have been this format YYYY-MM-DD HHMM_ExcelData.csv
+        filename = datetime.datetime.now().strftime("%Y-%m-%d %H%M_ExcelData.csv")
+        self.file_path = os.path.join(self.output_dir, filename)
+        with open(self.file_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Abs Time Vector', 'Relative Time', 'Events', 'Dose', 'Event Type', 'Selected Label', 'Score', 'Corrected label'])
 
     def relative_time_counter(self):
         if not hasattr(self, 'start_time'):
@@ -20,15 +29,7 @@ class EventLoggerCSV:
             current_time = datetime.datetime.now()
             relative_time = (current_time - self.start_time).total_seconds()
             return relative_time
-
-    def create_csv_file(self):
-        # filename must have been this format YYYY-MM-DD HHMM_ExcelData.csv
-        filename = datetime.datetime.now().strftime("%Y-%m-%d %H%M_ExcelData.csv")
-        self.file_path = os.path.join(DATA_DIR, filename)
-        with open(self.file_path, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(['Abs Time Vector', 'Relative Time', 'Events', 'Dose', 'Event Type', 'Selected Label', 'Score', 'Corrected label'])
-
+        
     def append_to_csv_file(self, event, dose, event_type, selected_label, score, corrected_label=None):
         abs_time_vector = datetime.datetime.now().strftime("%d-%b-%Y %H:%M:%S")
         relative_time = self.relative_time_counter()
