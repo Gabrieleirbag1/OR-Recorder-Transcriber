92feceec3a9ae364f8c0675651799cbe0ecdb64e
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-07-06 12:56:32 -0400

    refactor: implement CLI and GUI options

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import soundfile as sf
 import os
+import argparse
 from lite_logging.lite_logging import log
 from recorder import record_until_key_release
 from asr_text import AudioProcessor
@@ -21,8 +22,24 @@ def cli():
 
         log(f"Classification results: {result}")
 
+def gui():
+    import sys
+    from PyQt6.QtWidgets import QApplication
+    from gui import Window  
+
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+
 def main():
-    cli()
+    parser = argparse.ArgumentParser(description="Fine-tune a SetFit model on a training dataset")
+    parser.add_argument("--cli", "-c", type=str, default=None, help="Output name for the fine-tuned model")
+    args = parser.parse_args()
+    if args.cli:
+        cli()
+    else:
+        gui()
     
 if __name__ == "__main__":
     main()
